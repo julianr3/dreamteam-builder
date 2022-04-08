a14d45048b12d8e1ad6bf667d9e233385da9ceb3
Julian Richardson <jrichar3@yahoo.com>
2022-04-08 14:46:31 -0400

    started code for employee

---
+++ employee.js
@@ -0,0 +1,41 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+        
+    }
+}
+
+
+
+
+
+
+
+// The first class is an Employee parent class with the following properties and methods:
+
+// name
+
+// id
+
+// email
+
+// getName()
+
+// getId()
+
+// getEmail()
+
+// getRole()—returns 'Employee'
