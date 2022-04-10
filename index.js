9a497eb339ea45d74875ecd8877f693a6f9979b8
Julian Richardson <jrichar3@yahoo.com>
2022-04-10 19:20:54 -0400

    creating/edit prompt questions

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Manager = require("../lib/manager");
+const Engineer = require("../lib/engineer");
+const Intern = require("../lib/intern");
+const teamMembers = [];
 
-inquirer
-    .prompt([
+const promptManager = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -10,26 +14,31 @@ inquirer
         },
         {
             type: 'input',
-            name: 'name',
+            name: 'id',
             message: "What is the team manager's id?",
         },
         {
             type: 'input',
-            name: 'name',
+            name: 'email',
             message: "What is the team manager's email?",
         },
         {
             type: 'input',
-            name: 'name',
+            name: 'officeNum',
             message: "What is the team manager's office number?",
         },
         // team member questions
-        {
-            type: 'list',
-            name: 'name',
-            message: "Which type of team member would you like to add?",
-            choices: ['intern', 'engineer'],
-        },
+        // {
+        //     type: 'list',
+        //     name: 'name',
+        //     message: "Which type of team member would you like to add?",
+        //     choices: ['intern', 'engineer'],
+        // },
 
-        
-    ])
+       
+    ]).then(answers => {
+        console.log(answers);
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNum);
+        teamMembers.push(manager);
+    })
+}
